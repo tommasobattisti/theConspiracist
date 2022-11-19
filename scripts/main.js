c9f38abf46bcbb6e6af5a339203f72e1f07dcb35
Tommaso Battisti <tommaso.battisti4@studio.unibo.it>
2022-11-19 10:53:14 +0100

    Update style buttons to only structure with basic style

--- scripts/main.js
+++ scripts/main.js
@@ -14,12 +14,12 @@ window.onscroll = () => {
     }
 
 function changeBtnsVisibility() {
-    if (document.getElementsByClassName('btn-sub')[0].style.visibility == "hidden"){
-        for (let el of document.querySelectorAll('.btn-sub')){
+    if (document.getElementsByClassName('subBtn')[0].style.visibility == "hidden"){
+        for (let el of document.querySelectorAll('.subBtn')){
             el.style.visibility = 'visible';
             };
     }else{
-        for (let el of document.querySelectorAll('.btn-sub')){
+        for (let el of document.querySelectorAll('.subBtn')){
             el.style.visibility = 'hidden';
             };
     };
