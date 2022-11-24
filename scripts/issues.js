74a5a2f64cffc0f5e06a23efb5aa3366e18c5e32
Tommaso Battisti <82956419+tommasobattisti@users.noreply.github.com>
2022-11-24 11:22:52 +0100

    Update issues.js

--- scripts/issues.js
+++ scripts/issues.js
@@ -6,7 +6,7 @@ var listItemTpl = `<li><a href='#' onclick='loadDocument($url)'>$label</a></li>`
 function loadDocumentsList(){
     $.ajax({
         method: GET,
-        url: "https://github.com/tommasobattisti/streamlit-example/blob/master/ex.json",
+        url: "https://github.com/tommasobattisti/thePPPPconspiracy/blob/0bd9ced76b525bc649b52ebf2708cae55256507b/fileList.json",
         success: function(d){
             alert("WE");
             for (var i = 0; i<d.length; i++){
