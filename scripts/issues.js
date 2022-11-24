19706a34d9b9fef95e8d2daf7521d283dea72426
Tommaso Battisti <82956419+tommasobattisti@users.noreply.github.com>
2022-11-24 12:09:52 +0100

    Update issues.js

--- scripts/issues.js
+++ scripts/issues.js
@@ -1,17 +1,18 @@
 $(document).ready(loadDocumentsList);
 
 
-var listItemTpl = `<li><a href='#' onclick='loadDocument($url)'>$label</a></li>`
 
 function loadDocumentsList(){
     $.ajax({
-        method: GET,
-        url: "https://github.com/tommasobattisti/thePPPPconspiracy/blob/0bd9ced76b525bc649b52ebf2708cae55256507b/fileList.json",
-        success: function(d){
-            alert("WE");
-            for (var i = 0; i<d.length; i++){
-                $("#docs-list").append("<li>Bella li</li>");
-            }
+        method: "GET",
+        url: "https://raw.githubusercontent.com/tommasobattisti/thePPPPconspiracy/main/fileList.json",
+        success: function(listObj){
+            let parsedJson = JSON.parse(listObj)
+            console.log(parsedJson)
+            //parsedJson.forEach(obj => {
+                //console.log(obj)
+                //$("#docs-list").append('<li><a href="#" onclick="loadDoc('+obj.url+')">'+obj.label+'</a></li>)';
+            //})
         },
         error: function(){
             alert("No document to show")
