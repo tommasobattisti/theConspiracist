55b7cb623169376190b19287dff25720dce5da56
Tommaso Battisti <82956419+tommasobattisti@users.noreply.github.com>
2022-11-25 20:01:22 +0100

    Update link json file

--- scripts/issues.js
+++ scripts/issues.js
@@ -14,12 +14,27 @@ function loadDocumentsList(){
             } else {
                 console.log(parsedJson)
                 parsedJson.forEach(obj => {
-                    $("#docs-list").append('<li><a href="#" onclick="loadDoc('+obj.url+')">'+obj.label+'</a></li>'); // or: '<li class="doc-list-item" onclick="loadDoc('+obj.url+')">'+obj.label+'</li>' 
+                    $("#docs-list").append('<li><a href="#" onClick="loadDoc('+obj.url+')">'+obj.label+'</a></li>'); // or: '<li class="doc-list-item" onclick="loadDoc('+obj.url+')">'+obj.label+'</li>' 
                 });
             }
         },
         error: function(){
             alert("No document to show")
+        }
+    });
+}
+
+function loadDoc(file) {
+    $.ajax({
+        method: 'GET',
+        url: file,
+        success: function(d) {
+            alert("hey");
+            $('#file').html(d)
+            $('#title').html($('#file h1'))
+        },
+        error: function() {
+            alert('Could not load file '+file)
         }
     });
 }
